7d874156b69da3e035ca29375a72bc5f4cdf70de
SammarJuneja <sammarkumar345@gmail.com>
2024-10-18 16:00:59 +0530

    Fix typing indicator

--- src/routes/chat/[userid]/+page.svelte
+++ src/routes/chat/[userid]/+page.svelte
@@ -9,7 +9,8 @@
     let messages = [];
     let typingUser = null;
     let typingTimeout;
-    let username =  data.author.userGet.username;
+    let messOptions = false;
+    let username =  data.user.userGet.username;
 
     // Email: a1@gamil.com
     // Password: meowmeow1
@@ -34,6 +35,10 @@
         }
     }
 
+    function messageOptions() {
+        messOptions = !messOptions;
+    }
+
     function handleInput() {
         const newData = {
             chatId: data.chatId.chat[0]._id,
@@ -41,7 +46,7 @@
         }
         io.emit("typing", newData);
         clearTimeout(typingTimeout);
-        typingTimeout = setTimeout(clearTypingNotification, 2000);
+        typingTimeout = setTimeout(clearTypingNotification, 1000);
     }
 
     onMount(async() => {
@@ -68,30 +73,32 @@
 <div class="bg-app-bg min-h-screen">
     <div>
         <!-- header -->
-        <header class="bg-black gap-2 flex items-center text-white text-lg p-3">
+        <header class="bg-black gap-2 w-full fixed flex items-center text-white text-lg p-3">
             <img src={OpenChat} class="rounded-full border" width="35px" alt="Open Chat">
             <h2>{data.user.userGet.username || "Unknown user"}</h2>
+            {#if typingUser}
+                <p class="text-sm">typing...</p>
+            {/if}
         </header>
 
         <!-- main chat -->
-        <!-- {#if data.messages && data.messages.messages.length > 0} -->
-        <main class="flex-1 overflow-y-auto">
+        <main class="flex-1 pt-20 pb-36 overflow-y-auto">
             {#each messages as chat}
                 <div class={chat.sender === data.author.userGet._id ? "grid w-full justify-end text-right break-all pr-5" : "grid w-full justify-start text-left break-all pl-5"}>
                     <span>
                         <div class="my-2 ounded-md">
-                            <button class="bg-signup-button px-2 py-1 w-full max-w-52 rounded-lg break-all text-white">{chat.message}</button>
+                            <button on:click={messageOptions} class="bg-signup-button px-2 py-1 w-full max-w-52 rounded-lg break-all text-white">{chat.message}</button>
+                            {#if messOptions}
+                                <div class="bg-black text-white p-2">Delete</div>
+                            {/if}
                         </div>
                     </span>
                 </div>
             {/each}
         </main>
-        {#if typingUser}
-            <div class="text-gray-500 italic">{typingUser} is typing...</div>
-        {/if}
 
         <!--  bottom bar -->
-        <nav class="bottom-0 overflow-auto border-t w-full bg-app-bg">
+        <nav class="bottom-0 fixed overflow-auto border-t w-full bg-app-bg">
             <div class="flex gap-2 m-3 w-full">
                 <input bind:value={message} on:input={handleInput} class="rounded-full w-full text-white pl-3 outline-none bg-login-button" type="text" placeholder="Type your message here...">
                 <button class="p-1.5 bg-signup-button rounded-full">
@@ -109,12 +116,6 @@
                     <Icon icon="notification" size="20px" color="white"/>
                 </button>
                 <div class="bg-login-button flex gap-2 overflow-x-auto p-1 w-full rounded-t-md">
-                    <img src={OpenChat} class="rounded-full border" width="35px" alt="Open Chat">
-                    <img src={OpenChat} class="rounded-full border" width="35px" alt="Open Chat">
-                    <img src={OpenChat} class="rounded-full border" width="35px" alt="Open Chat">
-                    <img src={OpenChat} class="rounded-full border" width="35px" alt="Open Chat">
-                    <img src={OpenChat} class="rounded-full border" width="35px" alt="Open Chat">
-                    <img src={OpenChat} class="rounded-full border" width="35px" alt="Open Chat">
                     <img src={OpenChat} class="rounded-full border" width="35px" alt="Open Chat">
                     <img src={OpenChat} class="rounded-full border" width="35px" alt="Open Chat">
                 </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,12 +1,25 @@
 <script>
   import { goto } from "$app/navigation";
+  import Icon from "../../ui/Icon.svelte";
   
   let email = "";
   let password = "";
   let loginButton;
   let pass;
   let errorMessage = "";
+  let passIcon = "eye close";
   const apiUrl = "http://localhost:4000/api";
+
+  function showPass() {
+    let isPass = document.getElementById("password");
+    if (isPass.type === "password") {
+      passIcon = "eye open";
+      isPass.type = "text";
+    } else if (isPass.type === "text") {
+      passIcon = "eye close";
+      isPass.type = "password";
+    }
+  }
 
   async function login() {
     try {
@@ -43,7 +56,12 @@
   <div>
     <div class="bg-log-tab grid items-center justify-center p-10 rounded-lg w-60">
       <input on:keypress={(event) => { if (event.key === "Enter") { pass.focus(); } }} bind:value={email} class="bg-login-button rounded-lg text-white outline-none p-3 hover:border-2 hover:border-slate-400 hover:transition-all" placeholder="Email" type="text" required />
-      <input on:keypress={(event) => { if (event.key === "Enter") { loginButton.click(); } }} bind:this={pass} bind:value={password} class="bg-login-button rounded-lg text-white outline-none p-3 hover:border-2 hover:border-slate-400 hover:transition-all mt-5" placeholder="Password" type="password" required />
+      <span class="bg-login-button flex rounded-lg p-3 hover:border-2 hover:border-slate-400 hover:transition-all mt-5">
+        <input on:keypress={(event) => { if (event.key === "Enter") { loginButton.click(); } }} bind:this={pass} bind:value={password} class="text-white outline-none bg-login-button" placeholder="Password" id="password" type="password" required />
+        <button on:click={showPass}>
+          <Icon icon={passIcon} size="20px" />
+        </button>
+      </span>
     </div>
     <div class="grid mt-10 justify-center">
       <button bind:this={loginButton} on:click={login} class="p-2 w-40 bg-login-button text-white rounded-lg hover:bg-signup-button">
